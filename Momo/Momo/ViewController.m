555da8798d3c0e9355c2a5e2595c1db5fc190f6a
lxl <lxl@vip.com>
2016-09-22 11:30:08 +0800

    1.打印了一句话

--- Momo/Momo/ViewController.m
+++ Momo/Momo/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"打印一句话");
 }
 
 - (void)didReceiveMemoryWarning {
